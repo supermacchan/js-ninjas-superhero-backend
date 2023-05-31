55d2ecfa1781545dd45b2718a3f8429085aa4c62
Olga Birkovska <aiba.aki9@gmail.com>
2023-05-31 20:58:05 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
+
+// const contactsRouter = require('./routes/api/contacts');
+// const { authRouter } = require('./routes/api/auth');
+// const { errorHandler } = require('./helpers/apiHelpers');
+
+const app = express()
+
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+
+app.use(logger(formatsLogger))
+app.use(cors())
+app.use(express.json())
+app.use(express.static('public'))
+
+// app.use('/api/contacts', contactsRouter)
+// app.use('/api/users', authRouter)
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found' })
+})
+
+// app.use(errorHandler);
+
+module.exports = app
---
+++ server.js
@@ -0,0 +1,26 @@
+const app = require("./app");
+require('dotenv').config();
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3001;
+const uriDb = process.env.DB_HOST;
+console.log(uriDb);
+
+mongoose.set("strictQuery", true);
+
+const connection = mongoose.connect(uriDb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+connection
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Database connection successful");
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Server not running. Error message: ${err.message}`);
+    process.exit(1);
+  });
