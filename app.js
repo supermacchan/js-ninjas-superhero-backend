662235d59278d25a4259bff2480fcb4e94205ba5
Olga Birkovska <aiba.aki9@gmail.com>
2023-06-02 13:38:40 +0300

    swagger install

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger.json')
+
 const { heroRouter } = require('./routes/api/hero')
 
 const app = express()
@@ -14,6 +17,8 @@ app.use(express.json())
 app.use(express.static('public'))
 
 app.use('/api/heroes', heroRouter)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
