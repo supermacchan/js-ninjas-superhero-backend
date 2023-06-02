464bacc7010893d775bb1d33f47156f6562dbb35
Olga Birkovska <aiba.aki9@gmail.com>
2023-06-02 19:55:11 +0300

    fix update images issue

--- middleware/fileProcessingMiddleware.js
+++ middleware/fileProcessingMiddleware.js
@@ -2,19 +2,34 @@ const { ValidationError } = require('../helpers/errors');
 
 const fileProcessingMiddleware = async (req, res, next) => {
     try {
+        if (!req.body.img && req.files.length === 0) {
+            next();
+            return;
+        }
+
+        let images = []; 
+
+        if (req.body.img) {
+            if (typeof(req.body.img) === 'string') {
+                images.push(req.body.img);
+            } else {
+                images = req.body.img;
+            }
+        }
+
         if (req.files.length > 0) {
-            const images = [];  
             const files = req.files;
 
             for (let file of files) {
                 images.push(file.path);
             }
-
-            req.body.images = images;
         }
+
+        req.body.images = images;
 
         next();
     } catch (err) {
+        console.log(err);
         throw new ValidationError('Bad request.')
     }
 }
